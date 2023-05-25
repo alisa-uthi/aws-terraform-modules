ef560e92d9eb35f37297541023ade274eef827e3
Alisa <alisauthi@gmail.com>
2023-05-25 19:29:16 +0700

    remove subnet section

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -30,26 +30,6 @@ variable "azs" {
   type        = list(string)
 }
 
-# variable "private_subnets" {
-#   description = "CIDR range for private subnets"
-#   type        = list(string)
-# }
-
-# variable "public_subnets" {
-#   description = "CIDR range for public subnets"
-#   type        = list(string)
-# }
-
-# variable "private_subnet_names" {
-#   description = "Private subnet names"
-#   type        = list(string)
-# }
-
-# variable "public_subnet_names" {
-#   description = "Public subnet names"
-#   type        = list(string)
-# }
-
 variable "private_subnet_tags" {
   description = "Private subnet tags"
   type        = map(string)
