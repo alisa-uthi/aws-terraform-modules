1c9d42300ad01f86980a28808f67f7493b86b242
Alisa <alisauthi@gmail.com>
2023-05-25 19:49:10 +0700

    add azs to vpc module

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,6 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.2"
 
+  azs = var.azs
   private_subnets = [for k, v in var.azs : cidrsubnet(var.cidr, 8, k)]
   public_subnets  = [for k, v in var.azs : cidrsubnet(var.cidr, 8, k + 4)]
 }
