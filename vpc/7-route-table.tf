00f8a73b7ab550d0f48424dbd43bb3a2b107cf23
Alisa <alisauthi@gmail.com>
2023-05-27 21:52:41 +0700

    disable NAT gateway

--- vpc/7-route-table.tf
+++ vpc/7-route-table.tf
@@ -4,10 +4,12 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
   # For Private subnet to send all other subnet traffic to the NAT gateway
-  route {
-    cidr_block     = "0.0.0.0/0"
-    # nat_gateway_id = aws_nat_gateway.this.id
-  }
+  # route {
+  #   cidr_block     = "0.0.0.0/0"
+  #   nat_gateway_id = aws_nat_gateway.this.id
+  # }
+
+  route = []
 
   tags = merge(
     var.tags,
