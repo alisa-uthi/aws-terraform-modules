d29fc08d2025105eb474f717e322737e45f2336a
Alisa <alisauthi@gmail.com>
2023-05-29 11:11:45 +0700

    add S3 bucket module

---
+++ s3-bucket/1-variables.tf
@@ -0,0 +1,17 @@
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type = "string"
+  default = ""
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "force_destroy" {
+  description = "Boolean that indicates all objects should be deleted from the bucket when the bucket is destroyed"
+  type = bool
+  default = false
+}
