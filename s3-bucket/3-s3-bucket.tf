0012b3ca426707b9624dc5386f2d85fb7acbdfbf
Alisa <alisauthi@gmail.com>
2023-05-29 11:34:24 +0700

    fix bucket name issue

--- s3-bucket/3-s3-bucket.tf
+++ s3-bucket/3-s3-bucket.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "this" {
-  bucket        = "${var.cluster_state_bucket_name}"
+  bucket        = "${var.bucket_name}"
   force_destroy = var.force_destroy
   tags          = "${merge(var.tags)}"
 }
