4ec2329f353fe5c9763d939e55b4682475fafd4a
Alisa <alisauthi@gmail.com>
2023-05-29 18:32:03 +0700

    fix typo

--- vpc/2-outputs.tf
+++ vpc/2-outputs.tf
@@ -23,7 +23,7 @@ output "private_subnet_ids" {
   value       = aws_subnet.private[*].id
 }
 
-output "public_subnets_ids" {
+output "public_subnet_ids" {
   description = "List of IDs of public subnets"
   value       = aws_subnet.public[*].id
 }
