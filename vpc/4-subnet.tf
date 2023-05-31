d247033e40aa4841e5ffa8b22d5a0caf32848860
Alisa <alisauthi@gmail.com>
2023-05-31 16:19:40 +0700

    update VPC subnet

--- vpc/4-subnet.tf
+++ vpc/4-subnet.tf
@@ -11,10 +11,11 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.public_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 
   tags = merge(
     var.tags,
